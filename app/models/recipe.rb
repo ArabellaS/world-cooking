33fb3273885a353d586f82ea86c0b7b2f2ee37e7
Cecile Saiz <cecile.saiz@gmail.com>
2023-03-06 16:32:25 +0100

    Setup for WorldCooking with associations done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :place
+  has_many :categories, through: :tags
+  has_many :ingredients, through: :quantities
 
   validates :content, presence: true
 end
