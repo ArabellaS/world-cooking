5e4cf880a91aa4cc8131bf8d60ff6f42d5914fc6
Alexis Queune <alexis.queune1@gmail.com>
2023-03-08 16:54:48 +0100

    best is mahdi

--- app/services/scraper/recipes_by_country_service.rb
+++ app/services/scraper/recipes_by_country_service.rb
@@ -18,7 +18,6 @@ class Scraper::RecipesByCountryService
 
   def perform
     url = "https://www.allrecipes.com/recipes/#{@cuisine[@country][:code]}/world-cuisine/#{@cuisine[@country][:continent]}/#{@cuisine[@country][:name]}/"
-    p url
     html = URI.open(url).read
     doc = Nokogiri::HTML.parse(html, nil, "utf-8")
 
@@ -28,48 +27,52 @@ class Scraper::RecipesByCountryService
         name = element.search(".card__title-text").text.strip
 
         details_url = element.attribute("href").value
+        # photo_url = element.search('img').first.attributes["data-src"].value
+
+        # photo = URI.open(photo_url)
+
         details_doc = Nokogiri::HTML.parse(URI.open(details_url).read, nil, "utf-8")
 
-        # directions = details_doc.search('.comp.recipe__steps.mntl-block')[0]
+        directions = details_doc.search('.comp.recipe__steps.mntl-block')[0]
 
-        # steps = directions.search('li')
+        steps = directions.search('li')
 
-        # content = ""
+        content = ""
 
-        # steps.each_with_index do |step, index|
-        #   para = step.search('p')[0]
+        steps.each_with_index do |step, index|
+          para = step.search('p')[0]
 
-        #   content += "STEP #{index + 1}\n"
+          content += "STEP #{index + 1}\n"
 
-        #   content += para.text.strip
-        # end
+          content += para.text.strip
+        end
 
-        # Recipe.create(name: name, content: content, place: @place)
+        recipe = Recipe.create(name: name, content: content, place: @place)
+        # recipe.photo.attach(io: photo, filename: "#{recipe.name}_photo", content_type: 'image/*')
+
 
         directions_ingre = details_doc.search('.comp.mntl-structured-ingredients')[0]
         
         steps_ingre = directions_ingre.search('li')
 
         steps_ingre.each_with_index do |step, index|
-          name = step.search('span[data-ingredient-name="true"]')[0].text.strip
-          unit = step.search('span[data-ingredient-unit="true"]')[0].text.strip
-          amount = step.search('span[data-ingredient-quantity="true"]')[0].text.strip
+          name = step.search('span[data-ingredient-name="true"]')[0].try(:text).try(:strip)
+          unit = step.search('span[data-ingredient-unit="true"]')[0].try(:text).try(:strip)
+          amount = step.search('span[data-ingredient-quantity="true"]')[0].try(:text).try(:strip)
 
-          p name, unit, amount
+          # p name, unit, amount
 
-          # next if name.exists? && unit.exists? && amount.exists?
+          next unless name.present?
 
-          # if Ingredient.where(name: name).exists?
-          #   ingredient = Ingredient.where(name: name)
-          # else
-          #   ingredient = Ingredient.create(name: name)
-          # end
+          ingredient = Ingredient.find_or_create_by(name: name)
           # ## Comment accéder à la recipe ?
-          # Quantity.create(unit: unit, amount: amount, ingredient: ingredient)
+          Quantity.create(unit: unit, amount: amount, ingredient: ingredient, recipe: recipe)
         end
 
       end
+
     end
+    return true
   end
 end
 
