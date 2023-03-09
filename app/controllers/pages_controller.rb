89c18c8a6ec90fd5b80d2e9f7982277fb662d4cf
Labrousse Oussama <oussama.labrousse@yahoo.fr>
2023-03-09 17:15:24 +0100

    hello_user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,8 @@ class PagesController < ApplicationController
     else
       @recipes = Recipe.all
     end
+  end
 
+  def profile
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
   def show
+    @recipe = Recipe.find(params[:id])
   end
 end
