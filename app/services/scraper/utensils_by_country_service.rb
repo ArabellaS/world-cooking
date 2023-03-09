be60ccc706a7a3f059ff0f90ad3d5b47ab4c4b50
Alexis Queune <alexis.queune1@gmail.com>
2023-03-09 19:16:47 +0100

    finalize seed for utensils

--- app/services/scraper/utensils_by_country_service.rb
+++ app/services/scraper/utensils_by_country_service.rb
@@ -15,22 +15,76 @@ class Scraper::UtensilsByCountryService
   end
 
   def perform
-    url = @cuisine[@country][:url]
-    html = URI.open(url).read
-    doc = Nokogiri::HTML.parse(html, nil, "utf-8")
 
-    doc.search(".padding-0.callout-body").each do |element|
+    if @country == 'France'
 
-      steps = element.search(".txt--lang-native")
+      url = @cuisine[@country][:url]
+      html = URI.open(url).read
+      doc = Nokogiri::HTML.parse(html, nil, "utf-8")
 
-      steps.each_with_index do |step, index|
+      doc.search(".padding-0.callout-body").each do |element|
 
-        utensil_name = step.text.strip
+        steps = element.search(".txt--lang-native")
 
-        utensil = Utensil.find_or_create_by(name: utensil_name)
-        utensil.places << @place
+        steps.each_with_index do |step, index|
+
+          utensil_name = step.text.strip
+          utensil = Utensil.find_or_create_by(name: utensil_name)
+          utensil.places << @place
+
+        end
       end
 
+    else
+
+      if @country == 'Italy'
+
+        utensils = [ 
+          "Knives", 
+          "A Garlic Press", 
+          "Spoons: Wooden & Slotted",
+          "Colander: Scolapasta",
+          "A Cheese Grater",
+          "A Pasta Machine",
+          "A Pasta Drying Rack or Pasta Stand",
+          "A Ravioli Cutter, Press or Stamp"
+        ]
+
+      elsif @country == 'India'
+
+        utensils = [ 
+          "Uruli", 
+          "Murukku/Idiyappam Ural", 
+          "Appe Pan",
+          "Idli Cooker",
+          "Ammikallu or Silbatta",
+          "Clay Meen Chatti",
+          "Kalchatti",
+          "Brass Coffee Filter",
+          "Kuzhi Karandi",
+          "Soapstone Dosa Tawa"
+        ]
+
+      else #=> Japan
+        
+        utensils = [ 
+          "Fine Mesh Skimmer",
+          "Miso Strainer & Miso Muddler",
+          "Suribachi and Surikogi",
+          "Ceramic Grater",
+          "Otoshibuta/ Drop Lid",
+          "Hangiri/ Sushi Oke",
+          "Bamboo Sushi Mat",
+          "Long Chopsticks"
+        ]
+
+      end
+
+      utensils.each_with_index do |utensil, index|
+        utensil = Utensil.find_or_create_by(name: utensil)
+        utensil.places << @place
+      end
+    
     end
 
   return true
