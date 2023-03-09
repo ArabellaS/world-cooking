f887c1c806a9bb4dfc9cc595d18b446257dc48fa
Alexis Queune <alexis.queune1@gmail.com>
2023-03-09 16:47:19 +0100

    add real places to recipes, add japan recipes, add photo into recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :quantities
   has_many :categories, through: :tags
   has_many :ingredients, through: :quantities
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :purge
   validates :content, presence: true
 
   include PgSearch::Model
--- app/services/scraper/recipes_by_place_service.rb
+++ app/services/scraper/recipes_by_place_service.rb
@@ -11,18 +11,53 @@ class Scraper::RecipesByPlaceService
   end
 
   def perform
-    url = "https://wikipedia.org/wiki/#{formatted(@recipe[:name])}"
-    p url
-    html = URI.open(url).read
+
+    url = "https://en.wikipedia.org/wiki/#{formatted(@recipe[:name])}"
+
+    begin
+      response = URI.open(url)
+      puts "Doing some scrapping at #{url}..."
+    rescue
+      puts "The request for a page at #{url} timed out...exiting."
+      return false
+    end
+
+    html = response.read
+
     doc = Nokogiri::HTML.parse(html, nil, "utf-8")
 
-    doc.search(".infobox_v2.noarchive").each do |element|
+    # doc.search(".infobox_v2.noarchive").each do |element|
+    doc.search(".infobox.hrecipe.adr").each do |element|
 
-      p element
+      country = element.search(".infobox-data.country-name").text.strip
+      region = element.search(".infobox-data.region").text.strip
+
+      next unless country.present? && region.present?
+
+      ## Italy ##
+      region = "Campania, Calabria" if region == "CampaniaSicily  Apulia  Calabria"
+      region = "Veneto Friuli Venezia Giulia" if region == "VenetoFriuli Venezia Giulia"
+      region = "Prato (Tuscany)" if region == "Prato (Tuscany), Spain, Central America, the Philippines"
+
+      ## India ##
+      country = "India" if country == "Indian subcontinent"
+      region = "North India" if region == "North India, Indian subcontinent"
+      region = "North India" if region == "Northern region of the Indian subcontinent"
+      region = "Delhi" if region == "Delhi[1][2][3][4][5][6][7]"
+
+      ## Japan ##
+      country = "Canada" if country == "Canada, United States"
+
+      place = Place.find_or_create_by(country: country, region: region)
+
+      @recipe.place = place
+      @recipe.save
 
     end
     return true
   end
+
+  private
 
   def enlever_contenu_entre_parentheses(phrase)
     phrase.gsub(/\(.*?\)/, '')
@@ -43,7 +78,4 @@ class Scraper::RecipesByPlaceService
   def dernier_mot(phrase)
     phrase.split.last
   end
-
 end
-
-# Scraper::RecipesByPlaceService.call(Recipe)
