cfee7a838a7ceab34b4e22b03c231d749061b491
Alexis Queune <alexis.queune1@gmail.com>
2023-03-10 15:55:10 +0100

    add avatar-button at home page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import MenuController from "./menu_controller"
+application.register("menu", MenuController)
