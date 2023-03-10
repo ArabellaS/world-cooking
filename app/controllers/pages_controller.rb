14b904f2e8ae39a8f3bdc1dc8168e2b6a9a35b45
Labrousse Oussama <oussama.labrousse@yahoo.fr>
2023-03-10 11:30:27 +0100

    updatesearch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,19 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @place = Place.new
     if params[:query].present?
-      sql_query = <<~SQL
-        recipes.title @@ :query
-        OR recipes.name @@ :query
-        OR recipes.category @@ :query
-        OR recipes.place @@ :query
-      SQL
-      @recipes = Recipe.joins(:category, :place).where(sql_query, query: "%#{params[:query]}%")
+      @recipes = Recipe.global_search(params[:query])
+      @places = Place.where(id: @recipes.map(&:place).uniq!.map(&:id))
     else
       @recipes = Recipe.all
+      @places = Place.all
     end
-    @places = Place.all
     @markers = @places.geocoded.map do |place|
       {
         lat: place.lat,
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,2 @@
 class PlacesController < ApplicationController
-
-  def index
-    @places = Place.all
-    @markers = @places.geocoded.map do |place|
-      {
-        lat: place.latitude,
-        lng: place.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {place: place})
-        marker_html: render_to_string(partial: "marker")
-      }
-    raise
-    end
 end
