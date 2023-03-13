422814a5d9e270e805fbf80f4070732fd2b13df2
Cecile Saiz <cecile.saiz@gmail.com>
2023-03-13 18:01:36 +0100

    Compared flavor profiles

--- app/services/api/recipes_by_taste.rb
+++ app/services/api/recipes_by_taste.rb
@@ -1,29 +1,30 @@
 require "json"
 require "open-uri"
 
-class Api::RecipesByTasteService
-
+class Api::RecipesByTaste
   def initialize(recipe)
     @recipe = recipe
   end
 
   def self.call(recipe)
-    recipe.perform
+    new(recipe).perform
   end
 
   def perform
-
+    sleep(1)
     headers = {
       'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
       'x-rapidapi-key': ENV['SPOONACULAR_API_KEY'] }
     url = "https://api.spoonacular.com/recipes/complexSearch?apiKey=#{ENV['SPOONACULAR_API_KEY']}&query="
-    url_formatted = url + Recipe.last.name
+    url_formatted = url + I18n.transliterate(@recipe.name)
     recipe_serialized = URI.open(url_formatted).read
-    recipe_result = JSON(recipe_serialized)
-    id = recipe_result['results'][0]['id']
-    taste_url = "https://api.spoonacular.com/recipes/#{id}/tasteWidget.json?apiKey=#{ENV['SPOONACULAR_API_KEY']}"
-    recipe_serialized = URI.open(taste_url).read
-    recipe = JSON(recipe_serialized)
-    flavor_profile = @recipe.update(recipe.map { |_key, value| value = value / 20 })
+    recipe_result = JSON(recipe_serialized)['results'][0]
+    if recipe_result
+      id = recipe_result['id']
+      taste_url = "https://api.spoonacular.com/recipes/#{id}/tasteWidget.json?apiKey=#{ENV['SPOONACULAR_API_KEY']}"
+      recipe_serialized = URI.open(taste_url).read
+      recipe = JSON(recipe_serialized)
+      flavor_profile = @recipe.update(recipe.each { |key, value| recipe[key] = value / 20 })
+    end
   end
 end
--- app/services/comparison/comparing_recipe_to_flavor_profile.rb
+++ app/services/comparison/comparing_recipe_to_flavor_profile.rb
@@ -1,11 +1,23 @@
-class Comparison::
+class Comparison::ComparingRecipeToFlavorProfile
 
-  def initialize
+  def initialize(flavor_profile, recipe)
     @flavor_profile = flavor_profile
-    @flavor_profile.user = current_user
     @recipe = recipe
   end
 
-  def compare
+  def self.call(flavor_profile, recipe)
+    new(flavor_profile, recipe).perform
+  end
+
+  def perform
+    keys = ['saltiness', 'bitterness', 'savoriness', 'sourness', 'spiciness', 'sweetness', 'fattiness']
+
+    keys.each do |key|
+      grade = (@flavor_profile[key] - @recipe[key]) / 10
+
+      @flavor_profile[key] -= grade
+    end
+
+    @flavor_profile.save
   end
 end
